9bad5f2628727fc855fbf4ea6bba4aab9fa941f1
tmehault <47982179+Tmehault@users.noreply.github.com>
2019-03-01 11:06:31 +0100

    Update CartesianPath.py

--- CartesianPath.py
+++ CartesianPath.py
@@ -21,92 +21,78 @@ class MoveGroupPythonInterfaceTutorial(object):
   def __init__(self):
     super(MoveGroupPythonInterfaceTutorial, self).__init__()
 
-		## BEGIN_SUB_TUTORIAL setup
-		##
-		## First initialize `moveit_commander`_ and a `rospy`_ node:
+		# BEGIN_SUB_TUTORIAL setup
+		#
+		# First initialize `moveit_commander`_ and a `rospy`_ node:
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('move_group_python_interface_tutorial')
+  
+  #------------------------------------------------------------------------------
 
-		## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
-		## kinematic model and the robot's current joint states
+
+		# Instantiate a `RobotCommander`_ object. Provides information such as the robot's
+		# kinematic model and the robot's current joint states
     robot = moveit_commander.RobotCommander()
     #niryo = niryo_one_commander.ArmCommander()
 
-		## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
-		## for getting, setting, and updating the robot's internal understanding of the
-		## surrounding world:
+		# Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
+		# for getting, setting, and updating the robot's internal understanding of the
+		# surrounding world:
     scene = moveit_commander.PlanningSceneInterface()
 
-		## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-		## to a planning group (group of joints).  In this tutorial the group is the primary
-		## arm joints in the Panda robot, so we set the group's name to "panda_arm".
-		## If you are using a different robot, change this value to the name of your robot
-		## arm planning group.
-		## This interface can be used to plan and execute motions:
+		# Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+		# to a planning group (group of joints).  In this tutorial the group is the primary
+		# arm joints in the Panda robot, so we set the group's name to "panda_arm".
+		# If you are using a different robot, change this value to the name of your robot
+		# arm planning group.
+		# This interface can be used to plan and execute motions:
     group_name = "arm"
     move_group = moveit_commander.MoveGroupCommander(group_name)
+    
+        # Create a `DisplayTrajectory`_ ROS publisher which is used to display
+         # trajectories in Rviz:
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                   moveit_msgs.msg.DisplayTrajectory,
+                                                   queue_size=20)
+        # Get end effector link
+    self.end_effector_link = move_group.get_end_effector_link()
+        
+        # Set pose reference frame
+    #reference_frame = 'niryo_one'
+    #move_group.set_pose_reference_frame(reference_frame)
+       
+        # Set planning parameters
+    move_group.allow_replanning(True)
 
-    ##
-    ## Getting Basic Information
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^^
-    # We can get the name of the reference frame for this robot:
-    planning_frame = move_group.get_planning_frame()
-    print "============ Planning frame: %s" % planning_frame
-
-    # We can also print the name of the end-effector link for this group:
-    eef_link = move_group.get_end_effector_link()
-    print "============ End effector link: %s" % eef_link
-
-    # We can get a list of all the groups in the robot:
-    group_names = robot.get_group_names()
-    print "============ Available Planning Groups:", robot.get_group_names()
-
-    # Sometimes for debugging it is useful to print the entire state of the
-    # robot:
-    print "============ Printing robot state"
-    #print robot.get_current_state()
-    print ""
-    ## END_SUB_TUTORIAL
-
-    # Misc variables
-    self.box_name = ''
-    self.robot = robot
-    self.scene = scene
+    print("Successfully connected to move_group." +
+                "\n" + "Started group     : " + str(move_group.get_name()) + 
+                "\n" + "Planning_frame    : " + str(move_group.get_planning_frame()) + 
+                "\n" + "End effector link : " + str(self.end_effector_link))
+        
+    print("Arm Moveit Commander has been started")
+    
     self.move_group = move_group
-    #self.display_trajectory_publisher = display_trajectory_publisher
-    self.planning_frame = planning_frame
-    self.eef_link = eef_link
-    self.group_names = group_names
+    self.display_trajectory_publisher = display_trajectory_publisher
 
   def plan_cartesian_path(self, scale=1):
     # Copy class variables to local variables to make the web tutorials more clear.
     # In practice, you should use the class variables directly unless you have a good
     # reason not to.
         move_group = self.move_group
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
 
-    # BEGIN_SUB_TUTORIAL plan_cartesian_path
-    #
-    # Cartesian Paths
-    # ^^^^^^^^^^^^^^^
-    # You can plan a Cartesian path directly by specifying a list of waypoints
-    # for the end-effector to go through. If executing  interactively in a
-    # Python shell, set scale = 1.0.
-    #
         waypoints = []
 
-        wpose = move_group.get_current_pose().pose
+        wpose = move_group.get_current_pose().pose #recuperation pose actuelle
         print("\nPose :",wpose)
-        wpose.position.y += scale * 0.1  # First move (x)
-        wpose.position.y -= scale * 0.1  # and sideways (y)
-        waypoints.append(copy.deepcopy(wpose))
+    	print(n.get_arm_pose())
+        wpose.position.y += scale * 0.1 #remplacement par le 1er mvt pour OUBLIER LA POSE INITIALE (moveit bug)
+        wpose.position.y -= scale * 0.1
+        #waypoints.append(copy.deepcopy(wpose))
 
-        wpose.position.x += scale * 0.05  # Second move forward/backwards in (x)
+        wpose.position.x += scale * 0.05 
         waypoints.append(copy.deepcopy(wpose))
     
-        wpose.position.y -= scale * 0.1  # Third move sideways (y)
+        wpose.position.y -= scale * 0.1
         waypoints.append(copy.deepcopy(wpose))
 
 		# We want the Cartesian path to be interpolated at a resolution of 1 cm
@@ -114,34 +100,43 @@ class MoveGroupPythonInterfaceTutorial(object):
 		# translation.  We will disable the jump threshold by setting it to 0.0,
 		# ignoring the check for infeasible jumps in joint space, which is sufficient
 		# for this tutorial.
-        (plan, fraction) = move_group.compute_cartesian_path(
+        fraction=0.0
+        tries=0
+        t_deb=time.time()
+        while(fraction<1.0):
+         (plan, fraction) = move_group.compute_cartesian_path(
 										   waypoints,   # waypoints to follow
 										   0.01,        # eef_step
 										   0.0)         # jump_threshold
+         tries+=1
+         print("\t---try:"+str(tries)+"\t---completed:"+str(fraction*100)+"%")#affichage des iterations
+        t_fin=time.time()
+        print("==>  tries: "+str(tries)+" complete: "+str(fraction*100)+"%  in: "+str(t_fin-t_deb)+" sec")#recap du processus
 
-    # Note: We are just planning, not asking move_group to actually move the robot yet:
-        return plan #,fraction
+        return plan ,fraction
+
+
   def execute_plan(self, plan):
     # Copy class variables to local variables to make the web tutorials more clear.
     # In practice, you should use the class variables directly unless you have a good
     # reason not to.
     move_group = self.move_group
 
-    ## BEGIN_SUB_TUTORIAL execute_plan
-    ##
-    ## Executing a Plan
-    ## ^^^^^^^^^^^^^^^^
-    ## Use execute if you would like the robot to follow
-    ## the plan that has already been computed:
+    # BEGIN_SUB_TUTORIAL execute_plan
+    #
+    # Executing a Plan
+    # ^^^^^^^^^^^^^^^^
+    # Use execute if you would like the robot to follow
+    # the plan that has already been computed:
     move_group.execute(plan, wait=True)
 
-    ## **Note:** The robot's current joint state must be within some tolerance of the
-    ## first waypoint in the `RobotTrajectory`_ or ``execute()`` will fail
-    ## END_SUB_TUTORIAL
+    # **Note:** The robot's current joint state must be within some tolerance of the
+    # first waypoint in the `RobotTrajectory`_ or ``execute()`` will fail
+    # END_SUB_TUTORIAL
 
 tutorial = MoveGroupPythonInterfaceTutorial()
-cartesian_plan = tutorial.plan_cartesian_path()
-#print(cartesian_plan)
+cartesian_plan, fraction = tutorial.plan_cartesian_path()
+#print("\nTrajectoire--------------------\n",cartesian_plan)
 tutorial.execute_plan(cartesian_plan)
 
 print("\n====FINI\====\n")
