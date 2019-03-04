4ee4e1d28b0f82d9c6a44df9dc1008ed9b8299ea
tmehault <47982179+Tmehault@users.noreply.github.com>
2019-03-04 10:02:48 +0100

    fixed first point position
    getting the current pose by using get_current_pose() from move group object doesnt return the exact position. Using n.get_arm_pose() wich gets a RobotState msg directly from the publisher is a more suitable method (verified using niryo one studio)

--- CartesianPath.py
+++ CartesianPath.py
@@ -8,8 +8,9 @@ import moveit_msgs.msg
 import geometry_msgs.msg
 from math import pi
 from std_msgs.msg import String
-from moveit_commander.conversions import pose_to_list
-#import niryo_one_commander
+from niryo_one_msgs.msg import RobotState
+import niryo_one_commander
+from niryo_one_commander.position.position import Position
 
 from niryo_one_python_api.niryo_one_api import *
 import time
@@ -33,7 +34,6 @@ class MoveGroupPythonInterfaceTutorial(object):
 		# Instantiate a `RobotCommander`_ object. Provides information such as the robot's
 		# kinematic model and the robot's current joint states
     robot = moveit_commander.RobotCommander()
-    #niryo = niryo_one_commander.ArmCommander()
 
 		# Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
 		# for getting, setting, and updating the robot's internal understanding of the
@@ -49,18 +49,10 @@ class MoveGroupPythonInterfaceTutorial(object):
     group_name = "arm"
     move_group = moveit_commander.MoveGroupCommander(group_name)
     
-        # Create a `DisplayTrajectory`_ ROS publisher which is used to display
-         # trajectories in Rviz:
-    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                                   moveit_msgs.msg.DisplayTrajectory,
-                                                   queue_size=20)
+    
         # Get end effector link
     self.end_effector_link = move_group.get_end_effector_link()
-        
-        # Set pose reference frame
-    #reference_frame = 'niryo_one'
-    #move_group.set_pose_reference_frame(reference_frame)
-       
+    
         # Set planning parameters
     move_group.allow_replanning(True)
 
@@ -72,7 +64,11 @@ class MoveGroupPythonInterfaceTutorial(object):
     print("Arm Moveit Commander has been started")
     
     self.move_group = move_group
-    self.display_trajectory_publisher = display_trajectory_publisher
+    
+    
+    sub_pose = rospy.Subscriber('/niryo_one/robot_state', RobotState)
+    
+    
 
   def plan_cartesian_path(self, scale=1):
     # Copy class variables to local variables to make the web tutorials more clear.
@@ -82,9 +78,15 @@ class MoveGroupPythonInterfaceTutorial(object):
 
         waypoints = []
 
-        wpose = move_group.get_current_pose().pose #recuperation pose actuelle
+        wpose = move_group.get_current_pose().pose #recuperation pose actuelle ( -!!- differente de la vraie position: ecarts de pose)
+    	wpose.position.x=n.get_arm_pose().position.x #mise a jour avec les vraies valeurs de pose
+        wpose.position.y=n.get_arm_pose().position.y
+        wpose.position.z=n.get_arm_pose().position.z
+        wpose.orientation.x=n.get_arm_pose().rpy.roll
+        wpose.orientation.y=n.get_arm_pose().rpy.pitch
+        wpose.orientation.z=n.get_arm_pose().rpy.yaw
         print("\nPose :",wpose)
-    	print(n.get_arm_pose())
+        
         wpose.position.y += scale * 0.1 #remplacement par le 1er mvt pour OUBLIER LA POSE INITIALE (moveit bug)
         wpose.position.y -= scale * 0.1
         #waypoints.append(copy.deepcopy(wpose))
@@ -93,6 +95,9 @@ class MoveGroupPythonInterfaceTutorial(object):
         waypoints.append(copy.deepcopy(wpose))
     
         wpose.position.y -= scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))
+        
+        wpose.position.x -= scale * 0.05
         waypoints.append(copy.deepcopy(wpose))
 
 		# We want the Cartesian path to be interpolated at a resolution of 1 cm
@@ -104,40 +109,44 @@ class MoveGroupPythonInterfaceTutorial(object):
         tries=0
         t_deb=time.time()
         while(fraction<1.0):
-         (plan, fraction) = move_group.compute_cartesian_path(
-										   waypoints,   # waypoints to follow
-										   0.01,        # eef_step
-										   0.0)         # jump_threshold
+         (plan, fraction) = move_group.compute_cartesian_path(waypoints,0.001, 0.0)
+										      # waypoints to follow# eef_step# jump_threshold
          tries+=1
          print("\t---try:"+str(tries)+"\t---completed:"+str(fraction*100)+"%")#affichage des iterations
         t_fin=time.time()
-        print("==>  tries: "+str(tries)+" complete: "+str(fraction*100)+"%  in: "+str(t_fin-t_deb)+" sec")#recap du processus
+        c_time=t_fin-t_deb
+        print("==>  tries: "+str(tries)+" complete: "+str(fraction*100)+"%  in: "+str(c_time)+" sec")#recap du processus
 
-        return plan ,fraction
+        return plan , fraction, c_time
 
 
   def execute_plan(self, plan):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
     move_group = self.move_group
-
-    # BEGIN_SUB_TUTORIAL execute_plan
-    #
-    # Executing a Plan
-    # ^^^^^^^^^^^^^^^^
-    # Use execute if you would like the robot to follow
-    # the plan that has already been computed:
-    move_group.execute(plan, wait=True)
+    move_group.execute(plan,wait=True)
 
     # **Note:** The robot's current joint state must be within some tolerance of the
     # first waypoint in the `RobotTrajectory`_ or ``execute()`` will fail
     # END_SUB_TUTORIAL
+    
+    
+  def max_time_compute(self):#Only for benchmarking cartesian path method on 10 tries
+  
+   move_group=self.move_group
+   max_time=0
+   average=0
+   for i in range (0,10):
+    plan,fraction,c_time=tutorial.plan_cartesian_path()
+    average=average+c_time
+    if (c_time>max_time):
+     max_time=c_time
+   average=average/10
+   print("==> average time computation: "+str(average)+" sec | max time computation: "+str(max_time)) 
 
 tutorial = MoveGroupPythonInterfaceTutorial()
-cartesian_plan, fraction = tutorial.plan_cartesian_path()
-#print("\nTrajectoire--------------------\n",cartesian_plan)
+cartesian_plan, fraction,c_time = tutorial.plan_cartesian_path()
+#tutorial.max_time_compute()
+#print("\n",cartesian_plan)
+
 tutorial.execute_plan(cartesian_plan)
 
-print("\n====FINI\====\n")
-    # END_SUB_TUTORIAL
+print("\n====FIN\====\n")
