1e170e133abd9a0a7f57b6c09c1d2b5fee69c35f
tmehault <47982179+Tmehault@users.noreply.github.com>
2019-03-04 15:29:41 +0100

    First working version
    Programm is now able to execute trajectory from cartesian path on Niryo One arm
    <!>  Use this programm at your own risks, it was built with the assumption that the path choosen can't colide with anything, there is no checking.

--- CartesianPath.py
+++ CartesianPath.py
@@ -9,6 +9,9 @@ import geometry_msgs.msg
 from math import pi
 from std_msgs.msg import String
 from niryo_one_msgs.msg import RobotState
+from moveit_msgs.msg import  RobotState as RbState #changing name to avoid conflict with newly made RobotState msg by Niryo
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Header
 import niryo_one_commander
 from niryo_one_commander.position.position import Position
 
@@ -29,8 +32,6 @@ class MoveGroupPythonInterfaceTutorial(object):
     rospy.init_node('move_group_python_interface_tutorial')
   
   #------------------------------------------------------------------------------
-
-
 		# Instantiate a `RobotCommander`_ object. Provides information such as the robot's
 		# kinematic model and the robot's current joint states
     robot = moveit_commander.RobotCommander()
@@ -65,41 +66,94 @@ class MoveGroupPythonInterfaceTutorial(object):
     
     self.move_group = move_group
     
-    
     sub_pose = rospy.Subscriber('/niryo_one/robot_state', RobotState)
     
-    
-
-  def plan_cartesian_path(self, scale=1):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
+  def waypoints_path(self,scale=1): #This function is used to build a list of waypoints 
+        
         move_group = self.move_group
 
-        waypoints = []
-
-        wpose = move_group.get_current_pose().pose #recuperation pose actuelle ( -!!- differente de la vraie position: ecarts de pose)
-    	wpose.position.x=n.get_arm_pose().position.x #mise a jour avec les vraies valeurs de pose
+        # --- Getting current pose
+        wpose = move_group.get_current_pose().pose # ( -!!- values gave by MoveGroupCommander are different from true values)
+    	wpose.position.x=n.get_arm_pose().position.x #updating with true values from niryo 
         wpose.position.y=n.get_arm_pose().position.y
         wpose.position.z=n.get_arm_pose().position.z
         wpose.orientation.x=n.get_arm_pose().rpy.roll
         wpose.orientation.y=n.get_arm_pose().rpy.pitch
         wpose.orientation.z=n.get_arm_pose().rpy.yaw
-        print("\nPose :",wpose)
         
-        wpose.position.y += scale * 0.1 #remplacement par le 1er mvt pour OUBLIER LA POSE INITIALE (moveit bug)
-        wpose.position.y -= scale * 0.1
-        #waypoints.append(copy.deepcopy(wpose))
+        waypoints=[]
+        
+        # --- Adding points to follow in path
+        wpose.position.y += scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))   #need deepcopy to recursively copy all the data
 
-        wpose.position.x += scale * 0.05 
-        waypoints.append(copy.deepcopy(wpose))
-    
+        wpose.position.x += scale * 0.05    
         wpose.position.y -= scale * 0.1
         waypoints.append(copy.deepcopy(wpose))
         
         wpose.position.x -= scale * 0.05
+        wpose.position.y -= scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))
+        
+        wpose.position.x += scale * 0.05
+        wpose.position.y += scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))
+        
+        wpose.position.y += scale * 0.1
         waypoints.append(copy.deepcopy(wpose))
 
+        wpose.position.x += scale * 0.05    
+        wpose.position.y -= scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))
+        
+        wpose.position.x -= scale * 0.05
+        wpose.position.y -= scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))
+        
+        wpose.position.x += scale * 0.05
+        wpose.position.y += scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))
+        
+        wpose.position.y += scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))
+
+        wpose.position.x += scale * 0.05    
+        wpose.position.y -= scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))
+        
+        wpose.position.x -= scale * 0.05
+        wpose.position.y -= scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))
+        
+        wpose.position.x += scale * 0.05
+        wpose.position.y += scale * 0.1
+        waypoints.append(copy.deepcopy(wpose))
+        
+        print("Trajectory points found: "+str(len(waypoints)))
+        return waypoints
+
+  def plan_cartesian_path(self, waypoints): #This function is used to plan the path (calculating accelerations,velocities,positions etc..)
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
+        move_group = self.move_group
+        
+        
+        # --- Start state
+        joint_state = JointState()
+        joint_state.header = Header()
+        joint_state.header.stamp = rospy.Time.now()
+        joint_state.name = ['joint_1', 'joint_2','joint_3','joint_4','joint_5','joint_6']
+        joint_state.position = [n.get_joints()[0], n.get_joints()[1],n.get_joints()[2],n.get_joints()[3],n.get_joints()[4],n.get_joints()[5]]
+        moveit_robot_state = RbState()
+        moveit_robot_state.joint_state = joint_state
+        move_group.set_start_state(moveit_robot_state)
+        
+        
+        # --- Getting waypoints
+        
+        waypoints = waypoints
+        
 		# We want the Cartesian path to be interpolated at a resolution of 1 cm
 		# which is why we will specify 0.01 as the eef_step in Cartesian
 		# translation.  We will disable the jump threshold by setting it to 0.0,
@@ -110,7 +164,7 @@ class MoveGroupPythonInterfaceTutorial(object):
         t_deb=time.time()
         while(fraction<1.0):
          (plan, fraction) = move_group.compute_cartesian_path(waypoints,0.001, 0.0)
-										      # waypoints to follow# eef_step# jump_threshold
+										      # waypoints to follow# eef_step in meters# jump_threshold
          tries+=1
          print("\t---try:"+str(tries)+"\t---completed:"+str(fraction*100)+"%")#affichage des iterations
         t_fin=time.time()
@@ -119,34 +173,35 @@ class MoveGroupPythonInterfaceTutorial(object):
 
         return plan , fraction, c_time
 
-
-  def execute_plan(self, plan):
+  def execute_plan(self, plan): #This function is used to allow execution of the trajectory by Niryo arm
     move_group = self.move_group
+    t_deb=time.time()
     move_group.execute(plan,wait=True)
+    t_fin=time.time()
+    print("Started at: "+str(t_deb)+" elapsed time:"+str(t_fin-t_deb))
 
     # **Note:** The robot's current joint state must be within some tolerance of the
     # first waypoint in the `RobotTrajectory`_ or ``execute()`` will fail
     # END_SUB_TUTORIAL
     
     
-  def max_time_compute(self):#Only for benchmarking cartesian path method on 10 tries
+  def max_time_compute(self,waypoints,cycles): #This function is only used for benchmarking cartesian path method on 10 computation
   
    move_group=self.move_group
    max_time=0
    average=0
-   for i in range (0,10):
-    plan,fraction,c_time=tutorial.plan_cartesian_path()
+   for i in range (0,cycles):
+    plan,fraction,c_time=self.plan_cartesian_path(waypoints)
     average=average+c_time
     if (c_time>max_time):
      max_time=c_time
-   average=average/10
+   average=average/cycles
    print("==> average time computation: "+str(average)+" sec | max time computation: "+str(max_time)) 
 
-tutorial = MoveGroupPythonInterfaceTutorial()
-cartesian_plan, fraction,c_time = tutorial.plan_cartesian_path()
-#tutorial.max_time_compute()
-#print("\n",cartesian_plan)
+Instance = MoveGroupPythonInterfaceTutorial()
+way = Instance.waypoints_path()
+cartesian_plan, fraction,c_time = Instance.plan_cartesian_path(way)
+Instance.max_time_compute(way,30)
+#Instance.execute_plan(cartesian_plan)
 
-tutorial.execute_plan(cartesian_plan)
-
-print("\n====FIN\====\n")
+print("\n --- Program ended ---\n")
