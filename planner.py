350bdf64a03ec8bc8dc87ab257a3f5350051ae30
tmehault <47982179+Tmehault@users.noreply.github.com>
2019-05-09 11:51:16 +0200

    Now compatible with Rhino/grasshopper trajectories
    -Fixed quaternion to be used with Rhino/Grasshopper trajectories
    -Program now connects to cartesian service to get his trajectory

--- planner.py
+++ planner.py
@@ -1,39 +1,37 @@
 #!/usr/bin/env python
+#Modules
 import os
 import sys
 import copy
 import rospy
 import readline # autocompletion
-from progress.bar import FillingCirclesBar #progress bar
-from progress.bar import ChargingBar
+from progress.bar import FillingCirclesBar, ChargingBar #progress bar
 from multiprocessing import Process
 import signal
 import moveit_commander
-import geometry_msgs.msg
-from math import pi, sqrt, pow
-from std_msgs.msg import String
-from moveit_msgs.msg import  RobotState as RbState #changing name to avoid conflict with newly made RobotState msg by Niryo
-from moveit_msgs.msg import RobotTrajectory
-from sensor_msgs.msg import JointState
-from std_msgs.msg import Header
-from niryo_one_commander.position.position import Position
-import tf
-
-
-from niryo_one_python_api.niryo_one_api import *
 import time
+from math import pi, sqrt, pow
+from tf.transformations import *
+from niryo_one_python_api.niryo_one_api import *
+#Messages
+from std_msgs.msg import String, Header
+from moveit_msgs.msg import  RobotState as RbState #changing name to avoid conflict with newly made RobotState msg by Niryo
+from moveit_msgs.msg import RobotTrajectory, Constraints
+from moveit_msgs.srv import GetCartesianPath
+from sensor_msgs.msg import JointState
+
 
 n = NiryoOne()
 
-test_mode=0 #classic mode 0, test mode for benchmarking 1
+offsets_niryo=[0,0,0] #offsets for 2D+ printing (gcode origin: 0,0,0) [-0.89349, -0.08603, -0.332] 
 
 #colors
-cplanner='\033[104m'
+cplanner='\033[1;104m'
 cred='\033[91m'
 cend='\033[0m'
-cgreen='\033[92m'
-cyellow='\033[93m'
-cblue='\033[94m'
+cgreen='\033[1;42m'
+cyellow='\033[1;93m'
+cblue='\033[1;94m'
 
 dir=os.getcwd()+"/Trajectories"
 files=os.listdir(dir)
@@ -48,7 +46,7 @@ def completer(text, state):
 def time_bar(duration):
  t_in=time.time()
  t_actual=t_in
- state=ChargingBar('Progress',max=duration)
+ state=ChargingBar('Progress',max=duration-1)
  while(int(t_actual-t_in)<duration):
   time.sleep(1)
   t_actual=time.time()
@@ -67,13 +65,20 @@ class PlannerInterface(object):
     group_name = "arm"
     move_group = moveit_commander.MoveGroupCommander(group_name)
     
-        # Get end effector link
-    self.end_effector_link = move_group.get_end_effector_link()
-    print "end effector link :", self.end_effector_link
-    
         # Set planning parameters
     move_group.allow_replanning(True)
-        
+    
+    print("Successfully connected to move_group." +
+                "\n" + "Started group     : " + str(move_group.get_name()) + 
+                "\n" + "Planning_frame    : " + str(move_group.get_planning_frame()) + 
+                "\n" + "End effector link : " + str(move_group.get_end_effector_link()) +
+                "\n" + "Planning time : " + str(move_group.get_planning_time()) +
+                "\n" + "pose reference frame : " + str(move_group.get_pose_reference_frame()) +
+                "\n" + "goal joint tolerance : " + str(move_group.get_goal_joint_tolerance()) +
+                "\n" + "goal orientation tolerance : " + str(move_group.get_goal_orientation_tolerance()) +
+                "\n" + "path constraints : " + str(move_group.get_path_constraints()) +
+                "\n" + "goal position tolerance : " + str(move_group.get_goal_position_tolerance()) )
+
     print("Arm Moveit Commander has been started")
     self.move_group = move_group
     
@@ -83,15 +88,8 @@ class PlannerInterface(object):
 
         # --- Getting current pose
         wpose = move_group.get_current_pose().pose # ( -!!- values given by MoveGroupCommander are different from true values)
-    	wpose.position.x=n.get_arm_pose().position.x          #
-        wpose.position.y=n.get_arm_pose().position.y         #
-        wpose.position.z=n.get_arm_pose().position.z         # updating with true values from niryo 
-        wpose.orientation.x=n.get_arm_pose().rpy.roll        #
-        wpose.orientation.y=n.get_arm_pose().rpy.pitch      #
-        wpose.orientation.z=n.get_arm_pose().rpy.yaw       #
         
         waypoints=[]
-        waypoints.append(copy.deepcopy(wpose))#store current state to reduce jerk at the beginning
         
         # --- Adding points to follow in path
         print(cblue+"\n\t === Acquire a trajectory === \n"+cend)
@@ -101,11 +99,12 @@ class PlannerInterface(object):
         readline.set_completer(completer)#active autocompletion on filenames
         readline.parse_and_bind("tab: complete")
 
-        filename=raw_input("\nGetting path from file: ")
+        filename=raw_input(cyellow+"\nGetting path from file: "+cend)
         way=open("Trajectories/"+filename,'r')
         os.system('clear')
-        print("\t-> file : "+filename)
-              
+        print(cgreen+"\n\t   >  - -    Demonstrateur Niryo One - Surmoul 3D    - -  <   \n"+cend)
+        print("\n-> file : "+'\033[4m'+filename+cend)
+        print("\noffsets:"+offsets_niryo)      
         line=way.readline()
         nbr = 1  
         
@@ -118,50 +117,65 @@ class PlannerInterface(object):
           print line
           return 0
         way.seek(0)
-        i=0
-        print "lines:",nbr
         
-        extru=[0,0] #initialized with 2 zeros to prevent extrusion between start state and beginning of the real trajectory
-        
+        extru=[0] #initialized with zero to prevent extrusion between start state and beginning of the real trajectory
         traveling_distance=0 #to calculate travelling distance
         wpose.position.x=0  #reset pose
         wpose.position.y=0
         wpose.position.z=0
-        bar=FillingCirclesBar('Processing waypoints', max=nbr-1)
         
+        q2=quaternion_from_euler(pi/2,0,0)
+        
+
+        bar=FillingCirclesBar('Processing waypoints', max=nbr-1)
+        i=0
         while(i<=nbr-2):
          i+=1
          bar.next()
          tab=way.readline().split(' ')
-         traveling_distance += sqrt(pow(float(tab[0])-wpose.position.x,2) + pow(float(tab[1])-wpose.position.y,2) + pow(float(tab[2])-wpose.position.z,2))
+         if(extru[i-1]==1):
+          traveling_distance += sqrt(pow(float(tab[0])-wpose.position.x,2) + pow(float(tab[1])-wpose.position.y,2) + pow(float(tab[2])-wpose.position.z,2))
 
-         wpose.position.x=float(tab[0]) #- Position
-         wpose.position.y=float(tab[1])
-         wpose.position.z=float(tab[2]) 
+         wpose.position.x= float(tab[0]) + offsets_niryo[0]#- Position
+         wpose.position.y= float(tab[1]) + offsets_niryo[1]
+         wpose.position.z= float(tab[2]) + offsets_niryo[2]
        
-         wpose.orientation.x= float(tab[3]) #- Quaternion
-         wpose.orientation.y= float(tab[4])
-         wpose.orientation.z= float(tab[5])
-         wpose.orientation.w= float(tab[6])
+         q1=[ - float(tab[3]),float(tab[4]),float(tab[5]),float(tab[6])] #inverse quaternion
+         q3=quaternion_multiply(q1, q2) #then rotate
+         q3[0]=-q3[0] #invert it back
+         wpose.orientation.w= q3[0] #- Quaternion
+         wpose.orientation.x= q3[1]
+         wpose.orientation.y= q3[2]
+         wpose.orientation.z= q3[3]
+         
          extru.append(int(tab[7]))
          waypoints.append(copy.deepcopy(wpose))
-        #-- Outing trajectory
+         
+        #-- Outing trajectory---------------
         wpose.position.z+=0.1
+        wpose.orientation.x= 0 #- Quaternion
+        wpose.orientation.y= 0
+        wpose.orientation.z= 0
+        wpose.orientation.w= 1
         waypoints.append(copy.deepcopy(wpose))
         extru.append(0) #to stop extrusion at the end
         
         way.close()
         bar.finish()
-        
+        traveling_distance=round( traveling_distance / 2.1874 , 2) #ratio filament diameter 1.75mm / nozzle 0.8mm
         print("Trajectory points found: "+str(len(waypoints)))
-        print("Traveling distance: "+str(traveling_distance)+" meters")
+        print "Necessary Filament: %2dm %2dmm"%(divmod(traveling_distance,1000))
         return waypoints, extru
         
 
   def plan_cartesian_path(self, waypoints, start_joints): #This function is used to plan the path (calculating accelerations,velocities,positions etc..)
 
         move_group = self.move_group
-        print(cblue+"\n\t === Compute a trajectory ===\n"+cend)
+        
+        #check for robot stability 
+        if(n.get_learning_mode()):
+         print "Make sure that Niryo is not in learning mode and in a safe position close to where you want to print"
+         raw_input("Then press enter..")
         
         # --- Saving start state
         tab_joints=[start_joints[0], start_joints[1],start_joints[2],start_joints[3],start_joints[4],start_joints[5]]
@@ -176,64 +190,85 @@ class PlannerInterface(object):
         move_group.set_start_state(initial_state)
         
         
-        # --- Getting waypoints
+        # --- Getting waypoints localy
         waypoints = waypoints
 
         #-- Parameters
         fraction=0.0
         tries=0
-        max_tries=10
+        max_tries=5
         eef_step=1.0 #eef_step at 1.0 considering gcode is already an interpolation
-        velocity=0.03
-        print "Max tries authorized : ", max_tries, "\neef step : ", eef_step
+        velocity=1.0
         
+        print "\n --- Computing parameters ---"
+        print "| Max tries authorized : ", max_tries, "\n| Eef step : ", eef_step, "\n| Velocity : ",velocity*100,"%"
+        print " ------------------------------\n"
+        
+        try:
+            rospy.wait_for_service('compute_cartesian_path',2)
+        except (rospy.ServiceException, rospy.ROSException), e:
+            print("Service call failed:",e)
+            return None 
+        try:
+            moveit_cartesian_path = rospy.ServiceProxy('compute_cartesian_path', GetCartesianPath) #call cartesian service
+        except rospy.ServiceException,e:
+            print("Service call failed:",e)
+            return(None)
+            
         #-- Computation
+        best_frac=0.0
         t_in=time.time()
-        while(fraction<1.0 and tries<10):
-         (plan, fraction) = move_group.compute_cartesian_path(waypoints,eef_step, 0.0) 
-										      # waypoints to follow# eef_step in meters# jump_threshold
+        while(fraction<1.0 and tries<max_tries):
+         rospy.wait_for_service('compute_cartesian_path',2) #wait for service to be ready
+         response = moveit_cartesian_path(Header(), initial_state, 'arm', 'tool_link',waypoints, eef_step, 0.0, True, Constraints())
          tries+=1
+         fraction=round(response.fraction,5)
          if(fraction<1.0):
-          print(cred+"\t---try:"+str(tries)+"\t---completed:"+str(fraction*100)+"%"+cend)#printing iterations
+          print(cred+"---try:"+str(tries)+"\t---completed:"+str(fraction*100)+"%"+cend)#printing iterations
+          if(fraction>best_frac):#saving best plan
+           best_plan=response.solution
+           best_frac=response.fraction
          else:
-          print(cgreen+"\t---try:"+str(tries)+"\t---completed:"+str(fraction*100)+"%"+cend)#printing iterations
+          print("---try:"+str(tries)+"\t---completed:"+str(fraction*100)+"%")#printing iterations
+          best_plan=response.solution
+          best_frac=response.fraction
+          
+         time.sleep(1) #time sleep to cut CPU usage and let some cooling time
+          
         t_out=time.time()
-        c_time=t_out-t_in
-        print("==>  tries: "+str(tries)+" complete: "+str(fraction*100)+"%  in: "+str(c_time)+" sec")#process results
+        c_time=round(t_out-t_in,2)
+        print("\n==>  tries: "+str(tries)+" complete: "+str(best_frac*100)+"%  in: "+str(c_time)+" sec")#process results
         
         #-- Scaling speeds for Niryo One
-        print("Retiming trajectory at "+str(velocity*100)+"% speed..")
-        plan=move_group.retime_trajectory(initial_state, plan, velocity) #ref_state_in, plan, velocity sc
-        print("Done")
+        if(velocity<1.0):
+         print"==>  Retiming trajectory at ",velocity*100,"% speed..",
+         best_plan=move_group.retime_trajectory(initial_state, best_plan, velocity) #ref_state_in, plan, velocity sc
+         print "Done"
         
-        #-- Modification to allow stop&go extrusion on same point (same point=same timing)
-        # init_t=0
-        # for i in range(0,len(plan.joint_trajectory.points)):
-         # if(plan.joint_trajectory.points[i].time_from_start.nsecs==init_t):
-          # plan.joint_trajectory.points[i].time_from_start.nsecs+=1
-         # init_t=plan.joint_trajectory.points[i].time_from_start.nsecs
+        expect_m, expect_s = divmod( best_plan.joint_trajectory.points[-1].time_from_start.secs , 60)
+        expect_h, expect_m = divmod( expect_m  , 60)
+        print "\nExpected printing time : %dh%02dm%02ds" %(expect_h,expect_m,expect_s)
+        end_joints=list(best_plan.joint_trajectory.points[-1].positions)# returns last position in case of using multiple trajectories that are following each other
         
-        print "Expected printing time :", plan.joint_trajectory.points[-1].time_from_start.secs," secs",plan.joint_trajectory.points[-1].time_from_start.nsecs,"nsecs"
-        end_joints=list(plan.joint_trajectory.points[-1].positions)# returns last position in case of using multiple trajectories that are following each other
-        
-        return plan , end_joints
+        return best_plan , end_joints
 
   def execute_plan(self, plan): #This function is used to allow execution of the trajectory by Niryo arm
     move_group = self.move_group
-    
     t_in=time.time()
     print "Started at : ", time.asctime(time.localtime(t_in))
     move_group.execute(plan,wait=True)
     t_out=time.time()
     print "Finished at : ", time.asctime(time.localtime(t_out))
-    print "Elapsed : ", t_out-t_in
+    m,s=divmod(t_out-t_in,60)
+    h,m=divmod(m,60)
+    print "Elapsed : %dh%02dm%02ds" %(h,m,s)
     
   def timing_extrusion(self, extru, traj):
     tref=time.time()
     for i in range(0,len(extru)):
      t=tref + traj.points[i].time_from_start.secs + traj.points[i].time_from_start.nsecs * pow(10,-9)
-     if(traj.points[i].time_from_start.secs - traj.points[i-1].time_from_start.secs > 2):
-      time.sleep(traj.points[i].time_from_start.secs - traj.points[i-1].time_from_start.secs - 2) #sleep to reduce CPU usage when high duration between points
+     if(traj.points[i].time_from_start.secs - traj.points[i-1].time_from_start.secs > 1):
+      time.sleep(traj.points[i].time_from_start.secs - traj.points[i-1].time_from_start.secs - 0.5) #sleep to reduce CPU usage when high duration between points
      while( time.time() < (t)):
       pass
      n.digital_write(GPIO_1C, extru[i])
@@ -245,27 +280,35 @@ class PlannerInterface(object):
 #  --------   MAIN  --------------------
 #------------------------------------------------------------
 
-os.system('clear')
-print(cplanner+"\n\t   >  - -    Demonstrateur Niryo One - Surmoul 3D    - -  <   \n"+cend)
 Instance = PlannerInterface()
-way, extru = Instance.waypoints_path()
-
+quit=False
+while(not(quit)):
+ os.system('clear')
+ print(cgreen+"\n\t   >  - -    Demonstrateur Niryo One - Surmoul 3D    - -  <   \n"+cend)
+ way, extru = Instance.waypoints_path()
 # ---------------------------------------------------------------------------------------------------------------------------------
-if(not(test_mode) and way!=0):# Classic mode -----
- cartesian_plan, end_joints = Instance.plan_cartesian_path(way,n.get_joints())
- a=raw_input("Execute trajectory ? (yes/no) ")
- if(a=='yes'):
-  n.pin_mode(GPIO_1B,PIN_MODE_OUTPUT)
-  n.digital_write(GPIO_1B,1)
-  n.pin_mode(GPIO_1C,PIN_MODE_OUTPUT) 
-  job=Process(target=time_bar, args=(cartesian_plan.joint_trajectory.points[-1].time_from_start.secs,))
-  job2=Process(target=Instance.timing_extrusion, args=(extru, cartesian_plan.joint_trajectory,))
-  job.start()
-  job2.start()
-  Instance.execute_plan(cartesian_plan)
-  os.kill(int(job.pid), signal.SIGKILL) #kill process by the hard method (terminate and join doesnt work)
-  os.kill(int(job2.pid), signal.SIGKILL) #kill process by the hard method (terminate and join doesnt work)
+ 
+ if( way!=0):
+  cartesian_plan, end_joints = Instance.plan_cartesian_path(way,n.get_joints())
+  f=open('traj.txt','w')
+  f.write(str(cartesian_plan))
+  f.close()
+  a=raw_input(cyellow+"Execute trajectory ? (yes/no/quit) "+cend)
+  if(a=='yes'):
+   n.pin_mode(GPIO_1B,PIN_MODE_OUTPUT)
+   n.digital_write(GPIO_1B,1)
+   n.pin_mode(GPIO_1C,PIN_MODE_OUTPUT) 
+   job=Process(target=time_bar, args=(cartesian_plan.joint_trajectory.points[-1].time_from_start.secs,))
+   job2=Process(target=Instance.timing_extrusion, args=(extru, cartesian_plan.joint_trajectory,))
+   job.start()
+   job2.start()
+   Instance.execute_plan(cartesian_plan)
+   os.kill(int(job.pid), signal.SIGKILL) #kill process by the hard method (terminate and join doesnt work)
+   os.kill(int(job2.pid), signal.SIGKILL) #kill process by the hard method (terminate and join doesnt work)
+   quit=True
+  if(a=='quit'):
+   quit=True
   
 
 
-print(cgreen+"\n --- Program ended ---\n"+cend)
+print("\n"+cgreen+"\t\t --- Program end --- "+cend+"\n")
